1189806c7afdcb471ba099b215772f8d2e0e8ec5
SpraylnlPray <danielwildegger@gmx.de>
2020-04-08 16:52:52 +0200

    Basic Apollo Server, so far with generated resolvers and queries; added .env and .graphql files to git ignore, added temporary schema

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require( 'dotenv' )
+	.config();
+const express = require( 'express' );
+const { ApolloServer } = require( 'apollo-server-express' );
+const neo4j = require( 'neo4j-driver' );
+const cors = require( 'cors' );
+const { makeAugmentedSchema } = require( 'neo4j-graphql-js' );
+const typeDefs = require( './graphql-schema' );
+
+const app = express();
+app.use( cors() );
+
+const URI = `bolt://${ process.env.DB_HOST }:${ process.env.DB_PORT }`;
+const driver = neo4j.driver(
+	URI,
+	neo4j.auth.basic( process.env.DB_USER, process.env.DB_PW ),
+);
+
+const schema = makeAugmentedSchema( { typeDefs } );
+
+const server = new ApolloServer( {
+	context: { driver },
+	schema,
+	introspection: true,
+	playground: true,
+} );
+
+const port = process.env.PORT;
+const path = process.env.ENDPOINT;
+
+server.applyMiddleware( { app, path } );
+
+app.listen( { port, path }, () => {
+	console.log( `Server listening at http://localhost:${ port }${ path }` );
+} );
+
