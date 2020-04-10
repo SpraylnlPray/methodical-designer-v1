7000a79ff0ff9ef1b7cdd2298622bff0989a12c3
SpraylnlPray <danielwildegger@gmx.de>
2020-04-10 16:20:41 +0200

    Begin update link: Can update internal link properties, as well as to and from nodes

--- server/resolvers.js
+++ server/resolvers.js
@@ -50,7 +50,51 @@ const resolvers = {
 			let results = await session.run( query, args );
 			return results.records.map( record => record.get( 'n' ).properties )[0];
 		},
-		// update link
+		async UpdateLink( _, args, ctx ) {
+			// todo: maybe just on qargs arg for the whole scope?
+			const session = ctx.driver.session();
+			// only changed data will be sent - because of that I can check for existence in the props object
+			// if exists -> changed
+
+			// did "to" node change?
+			// delete current relation and set new relation
+			if ( args.props.to_id ) {
+				const qargs = { id: args.id, props: args.props };
+				const query = `
+					MATCH (l:Link)-[r:ON]->(:Node) WHERE l.id = $id
+					DELETE r
+					WITH l AS l
+					MATCH (to:Node) WHERE to.id = $props.to_id
+					CREATE (l)-[:ON]->(to)
+					RETURN l as link, to
+				`;
+				let results = await session.run( query, qargs );
+			}
+			// did "from" node change?
+			// delete current relation and set new relation
+			if ( args.props.from_id ) {
+				const qargs = { id: args.id, props: args.props };
+				const query = `
+					MATCH (:Node)-[r:ACTION]->(l:Link) WHERE l.id = $id
+					DELETE r
+					WITH l AS l
+					MATCH (from:Node) WHERE from.id = $props.from_id
+					CREATE (from)-[:ACTION]->(l)
+					RETURN l as link, from
+				`;
+				let results = await session.run( query, qargs );
+			}
+			// did sequence change? (updated, deleted, created)
+
+			// make sure all internal link props get updated
+			const qargs = { id: args.id, props: args.props };
+			const query = `
+				MATCH (l:Link) WHERE l.id = $id
+				SET l += $props
+				RETURN l as link
+			`;
+			let results = await session.run( query, qargs );
+		},
 		// delete node
 		// delete link
 	},
